9a298c96446cf7697656da681b53fa5b8a3a0242
Kirill <kirill_17@list.ru>
2017-08-31 04:50:20 +0300

    Students list added, registration updated

--- daolayer/src/main/java/com/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-import com.entities.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<UserRole, Long> {
-}
--- daolayer/src/main/java/com/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import com.entities.Students;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<Students, Long> {
-    Students findByUsername(String username);
-}
-
